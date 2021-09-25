fc3a1bd25b221a29504ca9bd1bf1d161685a64cf
highopes <36064931+highopes@users.noreply.github.com>
2021-09-25 12:12:11 +0800

    Add more samples

--- sdk-query2.py
+++ sdk-query2.py
@@ -4,7 +4,7 @@
 #                          weihang_hank@gmail.com                                 #
 ###################################################################################
 """
-If you are looking for a few useful Mo Query functions, look here
+This is a sample program for educational purposes ---- SDK Query section
 """
 import cobra.mit.access
 import cobra.mit.session
@@ -15,6 +15,7 @@ import cobra.model.vmm
 import requests
 import os
 from credentials import *
+from cobra.internal.codec.jsoncodec import toJSONStr
 
 # create a session
 requests.packages.urllib3.disable_warnings()
@@ -24,12 +25,20 @@ md = cobra.mit.access.MoDirectory(ls)
 md.login()
 
 
+# blocking = input("process is blocking\n")
+# print("input is " + blocking + "\n\n")
+
+
 def Query_Objs(class_name, wcard_str):
     """
     This function is to return object list that matches the classname and wcard str
     """
     class_query = cobra.mit.request.ClassQuery(class_name)
     class_query.propFilter = 'wcard({}.dn, "{}")'.format(class_name, wcard_str)
+    # class_query.subtree = "full"
+    # class_query.queryTarget = "subtree"
+    # class_query.queryTarget = "children"
+
     return md.query(class_query)
 
 
@@ -79,55 +88,123 @@ def test_input(_tenant, _aprofile, _epg):
 
 def get_BDname(_tenant, _aprofile, _epg):
     """
-    This function is to return bdname
+    This function is to return EPG's BD name
     """
     objs = Query_Objs("fvRsBd", "uni/tn-{}/ap-{}/epg-{}".format(_tenant, _aprofile, _epg))
     return objs[0].tnFvBDName
 
+
 def get_VMM(_tenant, _aprofile, _epg):
+    """
+    This function is to return EPG's VMM Domain's Dn (Only return the first one)
+    """
     objs = Query_Objs("fvRsDomAtt", "uni/tn-{}/ap-{}/epg-{}".format(_tenant, _aprofile, _epg))
     return objs[0].tDn
 
-def official_scoping(_tenant):
-    """
-    This function is using officially scoping method to return a obj list
-    """
-    app_query = cobra.mit.request.DnQuery('uni/tn-'+_tenant)
-    app_query.queryTarget = 'children'
-    # app_query.queryTarget = 'subtree'
-    app_query.classFilter = 'fvAp'
-    app_query.subtreeInclude = 'health'
-    # app_query.subtreeInclude = 'health,stats'
-    # app_query.subtree = 'full'
 
-    apps = md.query(app_query)
+def ap_health(_tenant):
+    """
+    This function is using general query scoping method to return App Profiles and their Health Scores
+    """
+    ap_query = cobra.mit.request.DnQuery('uni/tn-' + _tenant)
+    ap_query.queryTarget = 'children'
+    # app_query.queryTarget = 'subtree'
+    ap_query.classFilter = 'fvAp'
+    ap_query.subtreeInclude = 'health'
+    # ap_query.subtreeInclude = 'health,stats'
+
+    aps = md.query(ap_query)
 
     health_dict = {}
 
-    for app in apps:
-        # print(app.dn)
-        for health in app.children:
-            health_dict[app.name] = int(health.cur)
-            # health_dict[app.name] = health.cur
-            # print(health.__dict__)
+    for ap in aps:
+        for health in ap.children:
+            health_dict[ap.name] = int(health.cur)
+
     return health_dict
 
 
+def test_config(_obj):
+    """
+    This function is for test config
+    """
+    _newobj = cobra.model.fv.Ap(_obj, name="rogue_Ap", descr=u"test 8:57")
+    c = cobra.mit.request.ConfigRequest()
+    c.addMo(_obj)
+    md.commit(c)
+    return _newobj
+
+
+def test_del(_obj):
+    """
+    This function is for test config
+    """
+    print(_obj.name)
+    _obj.delete()
+    c = cobra.mit.request.ConfigRequest()
+    c.addMo(_obj)
+    md.commit(c)
+
+
+def print_json(_objs):
+    """
+    This function is to print objs
+    """
+    for _obj in _objs:
+        print(toJSONStr(_obj))
+
+
 def main():
-    # print(get_VMM("hangwe-tn", "hangwe-useg-ap", "cback-epg"))
-    # print(get_BDname("hangwe-tn", "hangwe-useg-ap", "cback-epg"))
+    # ACI Program Training:   General Query method, and query health score
+    print(ap_health('hangwe-tn'))
+
+    # # ACI Program Training:   Query by DN wildcard filter
+    # print_json(Query_Objs("fvTenant", "hangwe"))
+
+    # # ACI Program Training:   Query Rs... Mo
+    # print("The BD Name is " + get_BDname("hangwe-tn", "hangwe-outside-ap", "hangwe-DB-epg"))
+    # print("The 1st VMM Domain DN is " + get_VMM("hangwe-tn", "hangwe-outside-ap", "hangwe-web-epg"))
+
+    # # Config a Mo, but instead of building its father, querying its father
+    # aps = Query_Objs("fvAp", "uni/tn-hangwe-rogue/")
+    # print(toJSONStr(aps[0]))
+    # tns = Query_Objs("fvTenant", "uni/tn-hangwe-rogue")
+    # newobj = test_config(tns[0])
+    # print(toJSONStr(newobj))
+
+    # # Config a Mo, do not build anything, all by querying and modified its property
+    # aps = Query_Objs("fvAp", "uni/tn-hangwe-rogue/")
+    # print(toJSONStr(aps[0]))
+    # aps[0].descr = u"test 9:17"
+    # c = cobra.mit.request.ConfigRequest()
+    # c.addMo(aps[0])
+    # md.commit(c)
+    # print(toJSONStr(aps[0]))
+
+    # # toJSONStr can only print properties that necessary, but all properties inherent from Class still exist
+    # aps = Query_Objs("fvAp", "uni/tn-company01-hangwe/")
+    # print(toJSONStr(aps[0]))
+    # print("dn = " + str(aps[0].dn))
+    # print("modTs = " + aps[0].modTs)
+
+    # # Other Testing
     # test_input("hangwe-tn", "hangwe-useg-ap", "cback-epg")
     # print(get_tenant_list())
     # print(get_ap_list("hangwe-tn"))
     # print(get_epg_list("hangwe-tn", "hangwe-useg-ap"))
 
     # get_ap_list("hangwe-tn")
-    # objs = Query_Objs("fvAp", "tn-hangwe-tn")
+    # objs = Query_Objs("fvTenant", "uni/tn-hangwe-tn")
+
+    # newobj = test_config(objs[0])
+
+    # test_del(objs[0])
     # objs = official_scoping('hangwe-tn')
     # for obj in objs:
     #     print(obj.dn)
-    print(official_scoping('hangwe-tn'))
+
     # official_scoping('hangwe-tn')
+
 
 if __name__ == '__main__':
     main()
